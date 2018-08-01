2986a597cfddb6d90fcd3ffcf2e99a46e0f89ca1
tarasandrusiv <tarasandrusivkb24@gmail.com>
2018-08-01 10:18:07 +0000

    bad

--- js/index.js
+++ js/index.js
@@ -1,26 +1,17 @@
-const hello = 'Hello World';
-alert(hello);
-
-const userName = prompt ('Enter your name');
-const greetings = 'Hello, ' + userName;
-alert(greetings);
-
-const userAge = parseInt(prompt('Enter your age'));
-alert('Your age is: ' + userAge);
-
-const nextAge = userAge + 1;
-alert('Next year you will be: ' + nextAge);
 
 
-if (userAge >= 18) {
-    alert('You are adult');
-} else {
-    alert('You are child');
+const a = parseFloat(prompt('Enter coef a'));
+const b = parseFloat(prompt('Enter coef b'));
+const c = parseFloat(prompt('Enter coef c'));
+
+function quadr(a,b,c) {
+ const result =  a*a - b - c;
+ return result;
 }
 
-function add (a,b,c) {
-    const result = a + b + c ;
-    return result;
-}
-const result = add(2, 2, 2);
+const result = quadr(1,2,3);
 alert(result);
+
+if (result >= 1){
+    alert('false');
+}
